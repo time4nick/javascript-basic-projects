b40120ef4b1554b0b86ee9be2cabe580bb93ad73
time4nick <iracrand@gmail.com>
2022-02-04 16:56:23 -0600

    sxetup files

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const ranNum = getRan();
+  console.log(ranNum);
+  document.body.style.backgroundColor = colors[ranNum];
+  color.textContent = colors[ranNum];
+});
+function getRan() {
+  return Math.floor(Math.random() * colors.length);
+}
